f39cda6b27776a280427a7ee7ca15ee1661c99cb
Misha Rumbesht <m.rumbesht@gmail.com>
2017-10-30 13:27:40 +0100

    feat: Reintroduce initial state
    Add the initial state back

--- App.js
+++ App.js
@@ -18,34 +18,34 @@ function DecksStatusBar ({backgroundColor, ...props}) {
 }
 
 const initialState = {
-  // decksIds: [
-  //   'React',
-  //   'JavaScript',
-  // ],
-  // decks: {
-  //   React: {
-  //     title: 'React',
-  //     questions: [
-  //       {
-  //         question: 'What is React?',
-  //         answer: 'A library for managing user interfaces'
-  //       },
-  //       {
-  //         question: 'Where do you make Ajax requests in React?',
-  //         answer: 'The componentDidMount lifecycle event'
-  //       }
-  //     ]
-  //   },
-  //   JavaScript: {
-  //     title: 'JavaScript',
-  //     questions: [
-  //       {
-  //         question: 'What is a closure?',
-  //         answer: 'The combination of a function and the lexical environment within which that function was declared.'
-  //       }
-  //     ]
-  //   },
-  // },
+  decksIds: [
+    'React',
+    'JavaScript',
+  ],
+  decks: {
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces'
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
+        }
+      ]
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer: 'The combination of a function and the lexical environment within which that function was declared.'
+        }
+      ]
+    },
+  },
 };
 
 const store = configureStore(initialState);
